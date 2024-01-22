9db1bd49600375781546dd74d7e72be5072a7d4c
a.baryshev <a.baryshev@astondevs.ru>
2024-01-22 22:04:05 +0300

    v.0.2

--- sections.js
+++ sections.js
@@ -6,10 +6,23 @@ const descriptionButton = document.querySelector(".secion-me")
 const experienceButton = document.querySelector(".secion-experience")
 const contactsButton = document.querySelector(".section-contacts")
 
-const sections = {'Description': descriptionBlock, 'Experience': experienceBlock, 'Contacts': contactsBlock}
-const currentAppState = {currState: 'Description'}
+const myPhoto = document.querySelector(".my-photo")
+const myName = document.querySelector(".full-name")
+const myCareer = document.querySelector(".career")
 
-function openSection(appState, payload) {
+myPhoto.classList.add("photo-expand")
+myName.classList.add("name-expand")
+myCareer.classList.add("career-expand")
+
+const sections = {'Description': descriptionBlock, 'Experience': experienceBlock, 'Contacts': contactsBlock}
+const currentAppState = {currState: 'Description', 'currButton': descriptionButton}
+
+descriptionButton.classList.add("button-hover")
+
+function openSection(appState, payload, elem) {
+  if(appState.currState === payload) {
+    return
+  }
   switch(appState.currState) {
     case 'Description':
       descriptionBlock.style.display = 'none'
@@ -24,17 +37,44 @@ function openSection(appState, payload) {
       console.log('Error occurred. Please, check your CPU burden.')
   }
   sections[payload].style.display = 'flex'
+  changeScale(appState.currState, payload)
   appState.currState = payload
+  currentAppState.currButton.classList.remove("button-hover")
+  currentAppState.currButton = elem
+  currentAppState.currButton.classList.add("button-hover")
+}
+
+function changeScale(prev, curr) {
+  if((prev === 'Experience' || prev === 'Contacts') && (curr === 'Experience' || curr === 'Contacts')) {
+    return
+  } else if(prev === 'Description') {
+    myPhoto.classList.remove("photo-expand")
+    myName.classList.remove("name-expand")
+    myCareer.classList.remove("career-expand")
+
+    myPhoto.classList.add("photo-decrease")
+    myName.classList.add("name-decrease")
+    myCareer.classList.add("career-decrease")
+  } else if (prev === 'Experience' || prev === 'Contacts') {
+    myPhoto.classList.remove("photo-decrease")
+    myName.classList.remove("name-decrease")
+    myCareer.classList.remove("career-decrease")
+
+    myPhoto.classList.add("photo-expand")
+    myName.classList.add("name-expand")
+    myCareer.classList.add("career-expand")
+  }
+
 }
 
 descriptionButton.addEventListener('click', () => {
-  openSection(currentAppState, 'Description')
+  openSection(currentAppState, 'Description', descriptionButton)
 })
 
 experienceButton.addEventListener('click', () => {
-  openSection(currentAppState, 'Experience')
+  openSection(currentAppState, 'Experience', experienceButton)
 })
 
 contactsButton.addEventListener('click', () => {
-  openSection(currentAppState, 'Contacts')
+  openSection(currentAppState, 'Contacts', contactsButton)
 })
